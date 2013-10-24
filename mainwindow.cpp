eabc7d54b323bb14cc63e1a67158261f224db592
RCramiro <ramirochaidez@gmail.com>
2013-10-23 22:50:52 -0500

    Added Doxygen-style comments to everything

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,12 @@
+/**
+* @file
+* @author Ramiro Chaidez
+* @version 1.0
+* @section DESCRIPTION
+*  This file defines the MainWindow class. See MainWindow
+*  for more information.
+*/
+
 #include <iostream>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
@@ -14,17 +23,32 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
+/**
+ * Slot for pushButton::clicked() signals
+ *
+ * Prints a message to let user know that pushButton was clicked
+ */
 void MainWindow::on_pushButton_clicked()
 {
     std::cout << "pushbutton was clicked" << std::endl;
 }
 
+/**
+ * Slot for checkBox::toggled(bool) signals
+ * @param checked Indicates whether the checkBox was toggled to 'true' or 'false'
+ *
+ * Prints a message to let the user know that checkBox was toggled
+ */
 void MainWindow::on_checkBox_toggled(bool checked)
 {
     std::cout << "checkBox was toggled to " << (checked ? "true" : "false") << std::endl;
 }
 
+/**
+ * Slot for commandLinkButton::clicked() signals
+ *
+ * Prints a message to let the user know that commandLinkButton was clicked
+ */
 void MainWindow::on_commandLinkButton_clicked()
 {
     std::cout << "commandLinkButton was clicked" << std::endl;
--- mainwindow.h
+++ mainwindow.h
@@ -1,11 +1,12 @@
 /**
 * @file
-* @author Luc Renambot
+* @author Ramiro Chaidez
 * @version 1.0
-* @section LICENSE
-*  blah blah
 * @section DESCRIPTION
-*  blah blah blah
+*  This file declares the MainWindow class, which takes on the UI defined in ui, and
+*  is set to perform certain functions (on_pushButton_clicked(), on_checkBox_toggled(),
+*  or on_commandLinkButton_clicked()) whenever specific UI elements are clicked or
+*  toggled.
 */
 
 #ifndef MAINWINDOW_H
@@ -17,8 +18,8 @@ namespace Ui {
     class MainWindow;
 }
 
-/*! \brief Main class
- *         of my application for project CS340.
+/*! \brief Main class of the application. Is responsible for defining each UI
+ *         element's actions.
  *
  *  Inherits for QMainWindow from Qt
  */
@@ -26,22 +27,25 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-public:
-     /**
-      * Constructor for MainWindow
-      *
-      * @param parent a parent widget, can be null
-      */
-    MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    public:
+         /**
+          * Constructor for MainWindow
+          *
+          * @param parent The window's parent widget. This can be null if there is no parent
+          */
+        MainWindow(QWidget *parent = 0);
+        ~MainWindow();
 
-private slots:
-    void on_pushButton_clicked();
-    void on_checkBox_toggled(bool checked);
-    void on_commandLinkButton_clicked();
+    private slots:
+        void on_pushButton_clicked();
+        void on_checkBox_toggled(bool checked);
+        void on_commandLinkButton_clicked();
 
-private:
-    Ui::MainWindow *ui;
+    private:
+        /**
+         * The user interface used by MainWindow.
+         */
+        Ui::MainWindow *ui;
 };
 
 #endif // MAINWINDOW_H
